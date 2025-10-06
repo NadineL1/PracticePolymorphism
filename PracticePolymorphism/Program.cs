6a49dc5b2e8ed6cfc61f676dd40da15524f5b2dd
NadineL1 <nadine.sorinder@gmail.com>
2025-10-06 08:25:18 +0200

    Made objects from classes and used their Play method.

--- PracticePolymorphism/Program.cs
+++ PracticePolymorphism/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
+            Instrument instrument = new Instrument();
+            Drums drums = new();
+            Guitar guitar = new();
 
-            
+            instrument.Play();
+            drums.Play();
+            guitar.Play();
+
         }
         
     }
